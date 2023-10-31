2b869bb70aa379043423bfb4d5e4da0cbcaed534
maciejjablonsky <maciekjablonsky@gmail.com>
2023-10-31 20:56:14 +0100

    [hello triangle] clearing screen before event handling

--- src/main.cpp
+++ src/main.cpp
@@ -54,11 +54,11 @@ int main()
     {
         processInput(window);
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
     }
 
     glfwTerminate();
